ad6b11d21d29d17b217615b2518294c5ceef5409
Dixsionn <ns.ntnt7777@gmail.com>
2026-05-15 12:18:08 +0000

    Hook ksu

--- fs/exec.c
+++ fs/exec.c
@@ -1693,6 +1693,14 @@ extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *ar
 /*
  * sys_execve() executes a new program.
  */
+
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1705,8 +1713,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 	bool is_su;
 
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-
+   #ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+   #endif
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
